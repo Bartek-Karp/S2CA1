a1ccbe1eb95eecdb3d1352b02dac7482c0427c10
D00213436 <47351845+D00213436@users.noreply.github.com>
2019-02-23 11:15:53 +0000

    First commit. It's the skeleton code

---
+++ model/database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=d00213436';
+    $username = 'D00213436';
+    $password = 'JG$VbWmS';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+?>
